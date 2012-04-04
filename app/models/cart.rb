bb4cda41570409cd3d3b46a7cb77edb7d03b4be8
Chris Maddox <tyre77@gmail.com>
2012-04-04 19:55:24 -0400

    added cart/cart_item relationships

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,4 @@
 class Cart < ActiveRecord::Base
   attr_accessible :user_id
+  has_many :cart_items
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,3 +1,4 @@
 class CartItem < ActiveRecord::Base
   attr_accessible :cart_id, :price, :product_id, :quantity
+  belongs_to :cart
 end
