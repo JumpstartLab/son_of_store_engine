aec747e7e857cc7fb26f8d07d3e56b5a848a3a90
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-04-04 14:09:41 -0400

    Add/Edit for Products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,18 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    category = Category.new(params[:category])
+    category.save
+    redirect_to categories_path
+  end
+
+  def show
+    @category = Category.find_by_id(params[:id])
+  end
 end
