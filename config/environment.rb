3e6e72e58b7a2d2823f70dde85cea62482316857
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-04-12 10:21:28 -0400

    Removed bitly

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 StoreEngine::Application.initialize!
-
-Bitly.use_api_version_3
-BITLY = Bitly.new('dkaufman16', 'R_93a76875d901c6d76064f30a298f2791')
