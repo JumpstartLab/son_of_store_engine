12e818baf0d46e6c2cb40e533e982754afca5efd
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-04-17 20:59:34 -0400

    fixed ALL THE TESTS

--- spec/requests/products_requests_spec.rb
+++ spec/requests/products_requests_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe "Products Requests" do
+  let!(:admin_user) { Fabricate(:auth_user) }
+  before(:each) do
+    visit login_path
+    fill_in('Email', :with => admin_user.email)
+    fill_in('Password', :with => "admin")
+    click_button('Log in')
+  end
+
   context "root" do
     let!(:products) { [Fabricate(:product), Fabricate(:product)] }
 
@@ -56,17 +64,91 @@ describe "Products Requests" do
     it "shows the product title and price" do
       visit product_path(product)
       page.should have_content(product.title)
-      page.should have_content(product.price)
+      page.should have_content(product.decimal_price)
     end
   end
 
-  # context "new" do
-  #   it "should show a blank product form" do
-  #     visit new_product_path
-  #     page.should have_selector("input#product_title")
-  #     within("input#product_title") do
-  #       page.should have_content("")
-  #     end
-  #   end
-  # end
+  context "new" do
+    it "should show a blank product form" do
+      visit new_product_path
+      page.should have_selector("input#product_title")
+      within("input#product_title") do
+        page.should have_content("")
+      end
+    end
+  end
+
+  context "edit" do
+    let!(:product) { Fabricate(:product) }
+
+    it "should show a product form pre-filled with product's info" do
+      visit edit_product_path(product)
+      page.should have_selector("input#product_title")
+      page.should have_selector("input", :value => product.title)
+    end
+  end
+
+  context "update" do
+    let!(:product) { Fabricate(:product) }
+    let!(:category) { Fabricate(:category) }
+    before(:each) do
+      visit edit_product_path(product)
+    end
+    context "valid params are passed" do
+      it "updates the product" do
+        fill_in('Price', :with => 225)
+        check(category.name)
+        click_button("Update Product")
+        Product.find_by_id(1).price.should == 225
+      end
+    end
+    context "invalid params are passed" do
+      it "does not update the product" do
+        fill_in('Price', :with => -2)
+        check(category.name)
+        click_button('Update Product')
+        Product.find_by_id(1).price.should_not == -2
+      end
+    end
+  end
+
+  context "destroy" do
+    let!(:product) { Fabricate(:product) }
+    let!(:user) { Fabricate(:user, :is_admin => true) }
+    it "removes the product from the database" do
+      product_count = Product.all.count
+      ProductsController.any_instance.stub(:current_user).and_return(user)
+      visit product_path(product)
+      click_link('Delete')
+      Product.all.count.should == product_count - 1
+    end
+  end
+
+  context "create" do
+    let!(:category) { Fabricate(:category) }
+    before(:each) do
+      visit new_product_path
+    end
+    context "the params are valid for the product" do
+      it "creates a new product" do
+        product_count = Product.all.count
+        fill_in('Title', :with => 'Title')
+        fill_in('Description', :with => 'Description')
+        fill_in('Price', :with => 123)
+        check(category.name)
+        click_button(:submit)
+        Product.all.count.should == product_count + 1
+      end
+    end
+    context "the params are not valid" do
+      it "does not create a new product" do
+        product_count = Product.all.count
+        fill_in('Title', :with => 'Title')
+        fill_in('Description', :with => 'Description')
+        check(category.name)
+        click_button(:submit)
+        Product.all.count.should == product_count
+      end
+    end
+  end
 end
