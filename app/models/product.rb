121442e3b2d22ace27bee75e391ca877661528a0
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-04-19 10:35:57 -0400

    Product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,13 @@ class Product < ActiveRecord::Base
   has_many :order_items
   has_many :orders, :through => :order_items
   validates_presence_of :categories, :description, :title, :price
+  validates :title, :length => { :minimum => 1 }
+  validates :description, :length => { :minimum => 1 }
   validates_numericality_of :price, :greater_than => 0
+  validates_uniqueness_of :title
+  validates_format_of :image_url,
+                      :with => %r{.(gif|jpg|png)$}i, :allow_blank => true
+
   validate :categories_valid?
 
   default_scope order(:title) #orders them by title
