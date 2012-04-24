9a14fb2a2b1551368940369843291d0af0d5333b
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-04-24 15:42:43 -0400

    pushing store_id column

--- db/migrate/20120424192235_add_store_id_to_fucking_everything.rb
+++ db/migrate/20120424192235_add_store_id_to_fucking_everything.rb
@@ -1,5 +1,8 @@
 class AddStoreIdToFuckingEverything < ActiveRecord::Migration
   def change
-    
+    add_column :categories, :store_id, :integer
+    add_column :orders, :store_id, :integer
+    add_column :products, :store_id, :integer
+    add_column :sales, :store_id, :integer
   end
 end
