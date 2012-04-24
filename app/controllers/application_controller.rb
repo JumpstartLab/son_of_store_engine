81871223757a18985fc04a8e7a5f22b509e46036
Jeff Casimir <jeff@casimircreative.com>
2012-04-24 19:46:58 -0400

    CamelCaseMistake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :find_cart, :verify_user, :stripe_api_key
 
-  include authentication_helpers
-  include cart_helpers
+  include AuthenticationHelpers
+  include CartHelpers
 
 private
 
