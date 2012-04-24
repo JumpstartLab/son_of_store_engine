e28310605eafa6f44cf4b7dd255d234ff7e7633b
Jeff Casimir <jeff@casimircreative.com>
2012-04-24 19:17:34 -0400

    Reworking Admin namespacing and class definitions

--- app/controllers/store_admin/products_controller.rb
+++ app/controllers/store_admin/products_controller.rb
@@ -1,41 +1,43 @@
 # Restful actions for products
-class StoreAdmin::ProductsController < StoreAdminController
+module StoreAdmin
+  class ProductsController < Controller
 
-  def index
-    @products = Product.where(:active => 1).paginate(:page => params[:page])
-    @categories = Category.all
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(params[:product])
-    if @product.save()
-      redirect_to product_path(@product), :notice => "Product created."
-    else
-      render 'new'
+    def index
+      @products = Product.where(:active => 1).paginate(:page => params[:page])
+      @categories = Category.all
     end
-  end
 
-  def destroy
-    product = Product.find(params[:id])
-    product.destroy
-    redirect_to products_path, :notice => "Product Removed"
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:id])
-    if @product.update_attributes(params[:product])
-      redirect_to product_path(@product), :notice => "Product updated."
-    else
-      render 'edit'
+    def new
+      @product = Product.new
     end
-  end
 
+    def create
+      @product = Product.new(params[:product])
+      if @product.save()
+        redirect_to product_path(@product), :notice => "Product created."
+      else
+        render 'new'
+      end
+    end
+
+    def destroy
+      product = Product.find(params[:id])
+      product.destroy
+      redirect_to products_path, :notice => "Product Removed"
+    end
+
+    def edit
+      @product = Product.find(params[:id])
+    end
+
+    def update
+      @product = Product.find(params[:id])
+      if @product.update_attributes(params[:product])
+        redirect_to product_path(@product), :notice => "Product updated."
+      else
+        render 'edit'
+      end
+    end
+
+  end
 end
--- app/controllers/store_admin/users_controller.rb
+++ app/controllers/store_admin/users_controller.rb
@@ -1,13 +1,15 @@
 # Allows creating and modifying users
-class StoreAdmin::UsersController < StoreAdminController
+module StoreAdmin
+  class UsersController < Controller
 
-  def index
-    @users = User.all
-  end
+    def index
+      @users = User.all
+    end
 
-  def destroy
-    User.find(params[:id]).destroy
-    redirect_to users_path
+    def destroy
+      User.find(params[:id]).destroy
+      redirect_to users_path
+    end
+    
   end
-  
 end
