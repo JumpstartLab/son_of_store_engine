91becd4ebf937c6e8ffbb06f4ae0fa5ddd69c8a2
athal7 <andrew.thal@livingocial.com>
2012-04-25 18:43:23 -0400

    fix failing request spec

--- spec/requests/shopper/shopper_checkout_requests_spec.rb
+++ spec/requests/shopper/shopper_checkout_requests_spec.rb
@@ -71,8 +71,9 @@ describe "shopper checkout", :shopper => true do
 
       context "if the order has billing and shipping information" do
         it "can do two-click checkout" do
-          Order.any_instance.stub(billing_method_id: 1)
-          Order.any_instance.stub(shipping_address_id: 1)
+          billing = Fabricate(:billing_method)
+          shipping = Fabricate(:shipping_address)
+          Order.last.update_attributes(billing_method_id: billing.id, shipping_address_id: shipping.id)
           within "#cart-aside" do
             page.should have_content "Two-Click Check Out"
             click_link_or_button "Two-Click Check Out"
@@ -114,8 +115,9 @@ describe "shopper checkout", :shopper => true do
         end
         
         it "can check out" do
-          Order.any_instance.stub(billing_method_id: 1)
-          Order.any_instance.stub(shipping_address_id: 1)
+          billing = Fabricate(:billing_method)
+          shipping = Fabricate(:shipping_address)
+          Order.last.update_attributes(billing_method_id: billing.id, shipping_address_id: shipping.id)
           within "#main-content" do
             page.should have_content "Check Out"
             click_link_or_button "Check Out"
