4f51c85c3669cf17ff1aa30a9ff66e4128b4e7eb
Mike Chlipala <mchlipala@gmail.com>
2012-04-25 16:48:20 -0400

    Cart actions all work correctly now.

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -2,7 +2,8 @@ class CartProductsController < ApplicationController
 
   def new
     if current_cart.add_product_by_id(params[:product_id])
-      redirect_to cart_path(store, current_cart)
+      redirect_to :back, 
+        :notice => "Product added to cart!"
     else
       redirect_to cart_path(store, current_cart), 
         :notice => "This product has been retired."
@@ -11,12 +12,17 @@ class CartProductsController < ApplicationController
 
   def destroy
     current_cart.cart_products.find(params[:id]).destroy
-    redirect_to cart_path
+    #redirect_to cart_path(store, current_cart)
+    redirect_to :back,
+      :notice => "Product removed from cart!"
   end
 
   def update
-    current_cart_product = current_cart.cart_products.find(params[:id])
+    # there is a bug in here, it's passing product_id as url_name, need to change that
+    current_cart_product = current_cart.cart_products.find(params[:url_name])
     current_cart_product.update_quantity(params[:cart_product][:quantity])
-    redirect_to cart_path
+    #redirect_to cart_path
+    redirect_to :back,
+      :notice => "Your cart was updated!"
   end
 end
