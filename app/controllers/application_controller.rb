672f180bd0be5b972f1a6472c937f9f54e9ec90a
Jeff Casimir <jeff@casimircreative.com>
2012-04-25 10:23:15 -0400

    Before filter no longer needed with initializer in place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,8 @@
 # Base class that inherited by all other classes
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :find_cart, :verify_user, :stripe_api_key
+  before_filter :find_cart, :verify_user
 
   include AuthenticationHelpers
   include CartHelpers
-
-private
-
-  def stripe_api_key
-    Stripe.api_key = ENV['STRIPE_TOKEN'] if Rails.env.to_s == "production"
-  end
-
 end
