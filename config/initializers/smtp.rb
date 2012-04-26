5bdc6960aed5e98dc09b6c650c19f38c830b9728
Jonan Scheffler <jonanscheffler@gmail.com>
2012-04-26 12:36:04 -0400

    Uncomplicate the overcomplicated. (SMTP)

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => ENV['GMAIL_SMTP_DOMAIN'],
-  :user_name            => ENV['GMAIL_SMTP_USER'],
-  :password             => ENV['GMAIL_SMTP_PASSWORD'],
-  :enable_starttls_auto => true
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
 }
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.default_url_options[:host] = 'sonofstoreengine.com'
