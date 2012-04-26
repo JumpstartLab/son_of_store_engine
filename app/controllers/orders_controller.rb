8e5222565c80968e513e02208f96efbd44c3027b
Melanie Gilman <melanie.gilman@gmail.com>
2012-04-26 11:47:08 -0400

    Changed link and before filter

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 #
 class OrdersController < ApplicationController
   before_filter :lookup_order, :only => [:show, :edit, :update]
-  before_filter :require_order_user, only: [:show, :lookup]
+  before_filter :require_order_user, only: [:show]
 
   def index
     if params[:status] == "all"
