11d903b54776c9b77abd492fb9a2ce695b0b846d
michael verdi <michael.v.verdi@gmail.com>
2012-04-26 18:29:39 -0400

    removed space

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_filter :require_admin, :only => [:index,:destroy]
   before_filter :require_not_logged_in, :only => [:new, :create]
   before_filter :require_login, :only => [:edit, :update]
-  
+
   def new
     @user = User.new
   end
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       Resque.enqueue(NewUserEmailer, [@user.email, @user.name)
-      
       auto_login(@user)
       redirect_back_or_to root_url, :notice => "Account successfully made!"
     else
