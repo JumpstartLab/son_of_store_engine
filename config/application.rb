ca6b3f6a15f6e0c89976095eee3090f29db5d8ec
Jonan Scheffler <jonanscheffler@gmail.com>
2012-04-26 12:10:44 -0400

    More SMTPs

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,6 @@ module StoreEngine
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.action_mailer.delivery_method = :smtp
   end
 end
