a3790b3b4ffd0e23ecda04fb9692261c24f534b7
Melanie Gilman <melanie.gilman@gmail.com>
2012-04-26 07:50:24 -0400

    Undoing change

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,7 +5,7 @@ class OrderMailer < ActionMailer::Base
     @order = order
     @email = order.find_shipping.email_address
     #@url  = order_path(@order.store, @order.id)
-    @url = lookup_path(@order.store.to_param, @order.special_url)
+    @url = lookup_path(@order.store.to_param)
     mail(to: @email, subject: "Thank you for your order!")
   end
 end
