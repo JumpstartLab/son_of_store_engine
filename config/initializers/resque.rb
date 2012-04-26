2a4646f77c137f77ce78089524cd7e4c2a328335
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-04-26 18:21:08 -0400

    adding initializer for resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,3 +4,5 @@ ENV["REDISTOGO_URL"] ||= 'redis://redistogo:5c89ed218d04538e883296814b4a08f6@dru
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
