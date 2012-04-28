4250dc1cd5e50be86c261c9417f124f31dec5973
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-04-28 12:14:31 -0400

    fixing email delivery error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      Notification.sign_up_confirmation(@user).deliver
+      Notification.sign_up_confirmation(@user.email).deliver
       auto_login(@user)
       redirect_back_or_to root_url, :notice => "Account successfully made!"
     else
