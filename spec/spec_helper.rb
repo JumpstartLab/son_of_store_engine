0cdc0d1bbaade8fc2e2f34a2faca1fe595d1a2db
Mike Chlipala <mchlipala@gmail.com>
2012-04-28 18:09:27 -0400

    Updated spec_helper to create users before stores

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,19 +14,37 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.before(:suite) do
+    
+    FactoryGirl.create(:user,
+      :name => "Horace Worace",
+      :email => "horace@foo.com",
+      :display_name => "horace_worace",
+      :password => "foobar",
+      :password_confirmation => "foobar")
+
+    FactoryGirl.create(:user,
+      :name => "Harold",
+      :email => "harold@foo.com",
+      :display_name => "harold_harold",
+      :password => "foobar",
+      :password_confirmation => "foobar")
+
     FactoryGirl.create(:store,
      :name => "Best Sunglasses",
      :url_name => "best-sunglasses",
      :description => "errday im testin",
      :approved => true,
-     :enabled => true)
+     :enabled => true,
+     :owner_id => 1)
     
     FactoryGirl.create(:store,
      :name => "Test Store",
      :url_name => "test-store",
      :description => "errday im testin",
      :approved => true,
-     :enabled => true)
+     :enabled => true,
+     :owner_id => 2)
+
   end
 
   config.after(:suite) { Store.destroy_all }
