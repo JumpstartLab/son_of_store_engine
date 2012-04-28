69fef19c234476edc9f4388c20e504f938b6f3b8
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-04-28 15:28:34 -0400

    got validation of presence and uniqueness for slug and name

--- app/models/store.rb
+++ app/models/store.rb
@@ -7,6 +7,8 @@ class Store < ActiveRecord::Base
   has_many :orders
   has_many :categories
 
+  validates_presence_of :slug
+  validates_presence_of :name
   validates_presence_of :owner_id
   validates_uniqueness_of :name
   validates_uniqueness_of :slug
