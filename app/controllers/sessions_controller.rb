1536fb60d33620586d719aa2129152b3ed7038ac
Mike Chlipala <mchlipala@gmail.com>
2012-04-30 17:38:29 -0400

    Fixed all the signin behaviorgit add .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     cart = current_cart
+    last_page = session[:last_page]
     if user = login(params[:email], params[:password], params[:remember_me])
+      session[:last_page] = last_page
       successful_login(cart, user)
     else
       session[:cart_id] = cart.id
@@ -20,16 +22,6 @@ class SessionsController < ApplicationController
   end
 
 private
-
-  def successful_login(cart, user)
-    cart.assign_cart_to_user(user)
-    if session[:return_to_url]
-      redirect_to session[:return_to_url]
-    else
-      redirect_to stores_path,
-        :notice => "Logged in! Buy things! Capitalism!"
-    end
-  end
 
   def invalid_email
     flash.now.alert = "Email or password was invalid."
