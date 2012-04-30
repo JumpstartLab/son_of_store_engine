33678e84aaa2fe8cecfdad83419e68b515500cf1
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-04-30 10:13:16 -0400

    trying to fix redis...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,11 +36,20 @@ StoreEngine::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  # ActionMailer::Base.smtp_settings = {
+  #   :address  => "smtp.mailgun.org",
+  #   :port  => 25,
+  #   :user_name  => "postmaster@app4236527.mailgun.org",
+  #   :password  => "6pdqgiwcyfq2",
+  #   :authentication  => :login
+  # }
   ActionMailer::Base.smtp_settings = {
-    :address  => "smtp.mailgun.org",
-    :port  => 25,
-    :user_name  => "postmaster@app4236527.mailgun.org",
-    :password  => "6pdqgiwcyfq2",
-    :authentication  => :login
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => "app4236527@heroku.com",
+    :password       => "86gskmjs",
+    :domain         => 'heroku.com'
   }
+  ActionMailer::Base.delivery_method = :smtp  
 end
