63d12f172fde12fd8b1f6c91659b737ed15c41aa
Horace Williams <horace.d.williams@gmail.com>
2012-05-01 11:08:29 -0400

    approve/reject emails at store update; store_creation email after create

--- app/mailers/store_mailer.rb
+++ app/mailers/store_mailer.rb
@@ -1,9 +1,21 @@
 class StoreMailer < ActionMailer::Base
   default from: "confirmation@sonofstoreengine.herokuapp.com"
 
-  def store_approval_confirmation(user, store)
-    @user = User.find_by_id(store.owner_id)
-    @store = store
+  def store_approval_confirmation(user_id, store_id)
+    @user = User.find(user_id)
+    @store = Store.find(store_id)
     mail(to: @user.email, subject:"Your store has been approved!")
+  end
+
+  def store_rejection_confirmation(user_id, store_id)
+    @user = User.find(user_id)
+    @store = Store.find(store_id)
+    mail(to: @user.email, subject:"Your store has been rejected!")
+  end
+
+  def store_creation_confirmation(user_id, store_id)
+    @user = User.find(user_id)
+    @store = Store.find(store_id)
+    mail(to: @user.email, subject:"Your store has been created and is pending approval!")
   end
 end
