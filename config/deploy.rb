7b79c7bccfb45b7e91beacbf04975790b70e993e
Chris Maddox <tyre77@gmail.com>
2012-04-30 20:31:28 -0400

    changed repo to mikesea:master

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:tyre/#{application}.git"
+set :repository, "git@github.com:mikesea/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
