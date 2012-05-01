d82a51fb380ed5a47a7a7098d6b6157789150ccb
Jonan Scheffler <jonanscheffler@gmail.com>
2012-05-01 15:49:54 -0400

    Test SMTP in production.

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,19 +1,19 @@
-if ENV['HOST'] == 'sonofstoreengine'
+# if ENV['HOST'] == 'sonofstoreengine'
   ActionMailer::Base.smtp_settings = {
     :address        => '127.0.0.1',
     :port           => '25',
     :domain         => 'sonofstoreengine.com'
   }
-else
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
-  }
-end
+# else
+#   ActionMailer::Base.smtp_settings = {
+#     :address        => 'smtp.sendgrid.net',
+#     :port           => '587',
+#     :authentication => :plain,
+#     :user_name      => ENV['SENDGRID_USERNAME'],
+#     :password       => ENV['SENDGRID_PASSWORD'],
+#     :domain         => 'heroku.com'
+#   }
+# end
 
 delivery_method = if Rails.env.production?
                     :smtp
