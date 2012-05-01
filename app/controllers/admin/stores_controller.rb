7d06e207dde48c22609c351b40add492e3eebd12
Horace Williams <horace.d.williams@gmail.com>
2012-05-01 11:08:29 -0400

    approve/reject emails at store update; store_creation email after create

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -11,8 +11,10 @@ class Admin::StoresController < ApplicationController
   def update
     @store = Store.find_by_id(params[:id])
     @store.update_attributes(params[:store])
-    if params[:store][:approved => true]
-      StoreMailer.store_approval_confirmation(@store).deliver
+    if params[:store][:approved] == "true"
+      Resque.enqueue(Emailer, "store", "store_approval_confirmation", @store.owner.id, @store.id)
+    elsif params[:store][:approved] == "false"
+      Resque.enqueue(Emailer, "store", "store_rejection_confirmation", @store.owner.id, @store.id)
     end
     redirect_to :back,
       :notice => "#{@store.name} was updated!"
