4e649fdf59933a1f0921c01b1c2dbe1df01d7a8a
Mike Chlipala <mchlipala@gmail.com>
2012-05-01 00:04:08 -0400

    Super admin auth

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,7 +1,7 @@
 class Admin::StoresController < ApplicationController
   before_filter :require_login
-  before_filter :is_admin?, only: [ :index, :update ]
-  before_filter :is_store_admin?, only: [ :show ]
+  before_filter :is_super_admin
+  #before_filter :is_store_admin?, only: [ :show ]
   #before_filter :is_super_admin
 
   def index
@@ -20,6 +20,13 @@ class Admin::StoresController < ApplicationController
 
   def show
     @store = Store.find_by_url_name(params[:id])
+  end
+
+  private
+
+  def is_super_admin
+    redirect_to_last_page("Nice try, jerk.") unless
+      current_user.admin
   end
 
 end
---
+++ spec/requests/authorization_specs.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+  describe "As a visitor" do
+
+  end
+
+  describe "As a user" do
+
+  end
+
+  describe "As a store_admin" do
+
+  end
+
+  describe "As a store_stocker" do
+
+  end
+
+  describe "As a super_admin" do
+
+  end
