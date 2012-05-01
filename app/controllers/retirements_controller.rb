0c2deff5b897085ae82c8be1823da092be1b4518
Mike Chlipala <mchlipala@gmail.com>
2012-05-01 14:01:24 -0400

    Updating store stocker behavior

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -4,14 +4,16 @@ class RetirementsController < ApplicationController
     retirement = Retirement.new(params[:product_id])
     retirement.retire
 
-    redirect_to admin_products_path
+    redirect_to admin_products_path,
+      notice: "Product was retired!"
   end
 
   def update
     activate = Retirement.new(params[:product_id])
     activate.activate
 
-    redirect_to admin_products_path
+    redirect_to admin_products_path,
+      notice: "Product was un-retired!"
   end
 
 end
