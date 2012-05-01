d4c93d7653e4c5886466dbe0119f5fe8550ef280
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-05-01 18:09:18 -0400

    Fixed user mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,33 +10,33 @@ class UserMailer < ActionMailer::Base
 
   def invite_admin_email(store_id, admin_hex, email)
     @invite_code = admin_hex
-    @store_name = Store.find(store_id).name
-    mail(:to => email, :subject => "You have been invited to be an admin of #{@store_name}.")
+    store_name = Store.find(store_id).name
+    mail(:to => email, :subject => "You have been invited to be an admin of #{store_name}.")
   end
 
   def alert_admin_email(store_id, email)
     @store = Store.find(store_id)
-    mail(:to => email, :subject => "You have been added as an admin of #{@store_name}.")
+    mail(:to => email, :subject => "You have been added as an admin of #{@store.name}.")
   end
 
   def invite_stocker_email(store_id, admin_hex, email)
     @invite_code = admin_hex
-    @store_name = Store.find(store_id).name
-    mail(:to => email, :subject => "You have been invited to be a stocker of #{@store_name}.")
+    store_name = Store.find(store_id).name
+    mail(:to => email, :subject => "You have been invited to be a stocker of #{store_name}.")
   end
 
   def alert_stocker_email(store_id, email)
     @store = Store.find(store_id)
-    mail(:to => email, :subject => "You have been added as a stocker of #{@store_name}.")
+    mail(:to => email, :subject => "You have been added as a stocker of #{@store.name}.")
   end
 
   def fire_admin(store_id, email)
-    Store.find(store_id).name
-    mail(:to => email, :subject => "You have been fired from your admin position at #{@store_name}.")
+    store_name = Store.find(store_id).name
+    mail(:to => email, :subject => "You have been fired from your admin position at #{store_name}.")
   end
 
   def fire_stocker(store_id, email)
-    Store.find(store_id).name
-    mail(:to => email, :subject => "You have been fired from your stocker position at #{@store_name}.")
+    store_name = Store.find(store_id).name
+    mail(:to => email, :subject => "You have been fired from your stocker position at #{store_name}.")
   end
 end
