43b9cf8b3523155fa035a1e917701b181a5ef416
Melanie Gilman <melanie.gilman@gmail.com>
2012-05-01 11:28:04 -0400

    Add Resque mailer to production
    testing with redis to go
    
    Modify env queue in rake file
    
    Revert "Modify env queue in rake file"
    
    This reverts commit ba15edb20620e687c9fb3de55956aaf675d21f86.
    
    Add redistogo url to initializer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,8 @@
 require 'resque'
-Resque.redis = 'localhost:6379:1/myns'
+# Resque.redis = 'localhost:6379:1/myns'
+#
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:c71b799caebdb4d597de7487a2aac7dd@herring.redistogo.com:9910/"
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
