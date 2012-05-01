503ccced6c1c39769e3c5d79ec72c0d03ddc648d
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-05-01 18:35:14 -0400

    Fixed user mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,8 +14,9 @@ class UserMailer < ActionMailer::Base
     mail(:to => email, :subject => "You have been invited to be an admin of #{store_name}.")
   end
 
-
-  def alert_admin_email
+  def alert_admin_email(store_id, email)
+    @store = Store.find(store_id)
+    mail(:to => email, :subject => "You have been added as an admin of #{@store.name}.")
   end
 
   def invite_stocker_email(store_id, admin_hex, email)
