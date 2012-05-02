0c0978fc6b0c7abfa03374a18d177bfe19f8c9b8
Mike Chlipala <mchlipala@gmail.com>
2012-05-02 10:41:48 -0400

    Updated admin dashboard

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -4,4 +4,16 @@ module Admin::DashboardHelper
     OrderStatus.find_all_by_status(status).count
   end
 
+  def store_status
+    if store.pending?
+      "Pending approval"
+    elsif store.approved && store.enabled
+      "running live baby!"
+    elsif store.approved && store.disabled
+      "down for maintenance"
+    elsif store.declined
+      "declined. Go leave us alone"
+    end
+  end
+
 end
