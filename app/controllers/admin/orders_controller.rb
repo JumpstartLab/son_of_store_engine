be14b818ee3a3d8a6e8222a013038676124f5a07
Mike Chlipala <mchlipala@gmail.com>
2012-05-02 10:02:19 -0400

    Updating order routing

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,11 +5,15 @@ class Admin::OrdersController < Admin::ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])
+    @order = Admin::OrdersController.find_by_id_or_sha1(params[:id])
   end
 
   def update
-    @order = Order.find_by_id(params[:id])
+    @order = Admin::OrdersController.find_by_id_or_sha1(params[:id])
+  end
+
+  def self.find_by_id_or_sha1(id)
+    Order.find_by_id(id) || Order.find_by_sha1(id)
   end
 
 end
