f5a8e6b582a5f99a346a1988b2841f4a27ece29c
Austen Ito <austen.services@gmail.com>
2012-05-02 00:44:08 -0400

    Fixed fixnum

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -49,7 +49,7 @@ class StatsController < ApplicationController
 #name: 'John',
 			#data: [5, 3, 4, 7, 2]
     result = user_to_revenue.collect do |user_id, revenue| 
-      {:name => User.find(user_id).full_name, :data => [revenue}
+      {:name => User.find(user_id).full_name, :data => [revenue] }
     end
 
     render :json => result.to_json
