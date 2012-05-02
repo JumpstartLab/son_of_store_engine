a54227e4688537e9e82146905bc221eeb8ff422f
Jeff Casimir <jeff@jumpstartlab.com>
2012-05-01 23:55:47 -0400

    Tests back to green and zero Reek complaints

--- app/controllers/store_permissions_controller.rb
+++ app/controllers/store_permissions_controller.rb
@@ -2,17 +2,13 @@ class StorePermissionsController < ApplicationController
   before_filter :validate_added_email, only: :create
   before_filter :find_store, only: :destroy
 
-  def create
-    added_user = User.where(:email_address => params[:email]).first
-    store = Store.find(params[:store_permission][:store_id])
-    if added_user.nil?
+  def create    
+    if added_user
+      StorePermission.create_from_params_and_user(params[:store_permission], added_user)
+      notice = "#{added_user.full_name} has been given this role."
+    else
       StorePermission.invite_user_to_access_store(params[:store_permission], params[:email])
       notice = "#{params[:email]} is not currently a DOSE member. They have been invited to sign up to fulfill this role."
-    else
-      store_permission = StorePermission.new(params[:store_permission])
-      store_permission.user_id = added_user.id
-      store_permission.save
-      notice = "#{added_user.full_name} has been given this role."
     end
     redirect_to admin_store_path(store), notice: notice
   end
@@ -30,5 +26,13 @@ class StorePermissionsController < ApplicationController
     unless params[:email].match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/)
       redirect_to :back, notice: "Invalid Email"
     end
+  end
+
+  def added_user
+    @added_user ||= User.where(:email_address => params[:email]).first
+  end
+
+  def store
+    @store ||= Store.find(params[:store_permission][:store_id])
   end
 end
--- app/sweepers/product_sweeper.rb
+++ app/sweepers/product_sweeper.rb
@@ -15,16 +15,20 @@ class ProductSweeper < ActionController::Caching::Sweeper
 
   def expire_caches_for(product)
     slug = product.store.to_param
-    [slug, slug + "_admin"].each do |fragment|
-      expire_pages_with_key(category, fragment)
+    if slug
+      [slug, slug + "_admin"].each do |fragment|
+        expire_pages_with_key(product.store, fragment)
+      end
     end
   end
 
   def expire_category_caches_for(product)
-    product.categories.each do |category|
-      slug = category.store.to_param 
-      ["_category", "_admin_category"].each do |fragment|
-        expire_pages_with_key(category, slug + fragment)
+    slug = product.store.to_param
+    if slug
+      product.categories.each do |category|
+        ["_category", "_admin_category"].each do |fragment|
+          expire_pages_with_key(category, slug + fragment)
+        end
       end
     end
   end
