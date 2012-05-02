42163a2356f3f96cef7811a8ce887bd7633f7af9
Edward Weng <edweng@gmail.com>
2012-05-01 23:04:55 -0400

    Refactored and added style to admin dashboard

--- app/lib/find_by_status_extension.rb
+++ app/lib/find_by_status_extension.rb
@@ -1,10 +1,10 @@
 module FindByStatusExtension
   def find_by_status(status_filter=nil)
     if status_filter.nil?
-      self
+      self.order("created_at DESC")
     else
       self.joins(:order_status).
-        where('order_statuses.status = ?', status_filter)
+        where('order_statuses.status = ?', status_filter).order("created_at DESC")
     end
   end
 end
