e702489cda0c4037d32839e0a39863469145b8ad
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-05-01 22:25:29 -0400

    updating emailers

--- app/jobs/new_user_and_store_role_emailer.rb
+++ app/jobs/new_user_and_store_role_emailer.rb
@@ -2,6 +2,6 @@ class NewUserAndStoreRoleEmailer
   @queue = :emailer
 
   def self.perform(email, store_id, role)
-    Notification.new_store_role(email, store_id, role).deliver
+    Notification.new_user_and_store_role(email, store_id, role).deliver
   end
 end
