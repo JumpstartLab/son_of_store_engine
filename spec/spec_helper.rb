a4d5e0079a7a57b0b076b3b3c6d490b2c6974388
Mike Chlipala <mchlipala@gmail.com>
2012-05-01 23:24:09 -0400

    Ignore testing the seeder file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start do
+  'rails'
+  add_filter '/lib/seeder.rb'
+end
 
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
